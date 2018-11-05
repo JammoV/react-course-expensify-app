50cb0b1a8b2cb47d2c2f3db243a6d28eac5b24fd
Jarmo <jarmovleeshouwer@gmail.com>
2018-11-05 21:20:29 +0100

    Fix tests and update packages

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import LoginPage from '../../components/LoginPage';
+import { LoginPage } from '../../components/LoginPage';
 
-test('should render LoginPage correctly', () => {
-    const wrapper = shallow(<LoginPage />);
-    expect(wrapper).toMatchSnapshot();
+test('should correctly render LoginPage', () => {
+  const wrapper = shallow(<LoginPage />);
+  expect(wrapper).toMatchSnapshot();
 });
 
-test('shoud call startLogin on button click', () => {
-    const startLogin = jest.fn();
-    const wrapper = shallow(<LoginPage startLogin={startLogin} />);
-    wrapper.find('button').simulate('click');
-    expect(startLogin).toHaveBeenCalled();
+test('should call startLogin on button click', () => {
+  const startLogin = jest.fn();
+  const wrapper = shallow(<LoginPage startLogin={startLogin} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogin).toHaveBeenCalled();
 });
